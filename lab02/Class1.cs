926e631a082778a536c3efd32e58e5330a4b6e07
harpr <harpr@SARABAVI>
2024-02-19 22:56:25 -0500

    Add project files.

---
+++ lab02/Class1.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+namespace NUnitDemonstration.Tests
+{
+    [TestFixture]
+    public class TriangleTests
+
+    {
+        [Test]
+        public void ValidTriangle_Mutation1_ReturnsvalidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is invalid.", result);
+        }
+        [Test]
+        public void ValidTriangle_InvalidInput_ReturnsInvalidMessage1()
+        {
+
+
+            // Arrange
+            int firstAngle = 50;
+            int secondAngle = 60;
+            int thirdAngle = 120;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+
+        }
+
+        [Test]
+        public void ValidTriangle_InvalidInput_ReturnsInvalidMessage2()
+        {
+            // Arrange
+            int firstAngle = -70;
+            int secondAngle = 60;
+            int thirdAngle = 20;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+        }
+        [Test]
+        public void ValidTriangle_InvalidInput_ReturnsInvalidMessage3()
+        {
+            // Arrange
+            int firstAngle = 0;
+            int secondAngle = 60;
+            int thirdAngle = 20;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+        }
+        [Test]
+        public void ValidTriangle_InvalidInput_ReturnsInvalidMessage4()
+        {
+            // Arrange
+            int firstAngle = 70;
+            int secondAngle = 50;
+            int thirdAngle = 70;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+        }
+        [Test]
+        public void ValidTriangle_InvalidInput_ReturnsInvalidMessage5()
+        {
+            // Arrange
+            int firstAngle = 0;
+            int secondAngle = 0;
+            int thirdAngle = 0;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+        }
+    }
+}
---
+++ lab02/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            //harpreetkaur
+            //mutant 3:changing datatype int to double
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is not valid.";
+            }
+            return result;
+        }
+    }
+}
